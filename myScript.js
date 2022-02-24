ca85aca8f052fc1ea99844b2cfea149970d857f7
Irfanbd <gourob83@gmail.com>
2022-02-24 11:12:58 +0600

    update

--- myScript.js
+++ myScript.js
@@ -1,4 +1,13 @@
-var text = prompt("Enter Your Name");
-var len = text.length;
-document.write("Number of Character<br> " + text.length +"<br>" );
-document.write(text.charAt(2));
+// var text = prompt("Enter Your Name");
+// var len = text.length;
+// document.write("Number of Character<br> " + text.length +"<br>" );
+// document.write(text.charAt(2));
+
+
+
+// var text1 = "Bangladesh";
+// var text = text1.slice(2, 5);
+// document.write(text);
+var a = 4;
+a--;
+console.log(a);
