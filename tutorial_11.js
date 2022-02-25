3a624b2f25f7e604e158c9ccc98a6234121ce664
Irfanbd <gourob83@gmail.com>
2022-02-25 07:26:37 +0600

    update

---
+++ tutorial_11.js
@@ -0,0 +1,20 @@
+var base = parseFloat(prompt("Enter Base "));
+var height =parseFloat(prompt("Enter Height")) ;
+
+var area = square, rectangle, triangle, parallelogram, rhombus, kite;
+square = base * height;
+document.write("Area of square : = " + square + "</br>");
+rectangle = base * height;
+document.write("Area of rectangle : = " + rectangle + "</br>");
+triangle = base * height / 2;
+document.write("Area of triangle : = " + triangle + "</br>");
+parallelogram = base * height;
+document.write("Area of parallelogram : = " + parallelogram + "</br>");
+rhombus = base * height / 2;
+document.write("Area of rhombus : = " + rhombus + "</br>");
+kite = base * height / 2;
+document.write("Area of kite : = " + kite + "</br>");
+
+
+
+
