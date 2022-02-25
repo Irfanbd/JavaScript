b763d62a5b58fde03079e893a68ea884d6c825b4
Irfanbd <gourob83@gmail.com>
2022-02-26 02:47:30 +0600

    update

---
+++ tutorial_18.js
@@ -0,0 +1,4 @@
+for (var i = 1; i <= 10;i++)
+{
+    document.write( i +" <h2>Learn , Bug , Fail , Learn Again </br></h2>")
+}
---
+++ tutorial_19.js
@@ -0,0 +1,7 @@
+var sum = 0;
+for (var i = 1; i <= 10; i = i + 1){
+    sum = sum + i;
+    // document.write(" Total : " + sum+"</br>");
+   
+}
+document.write(" Total : " + sum);
