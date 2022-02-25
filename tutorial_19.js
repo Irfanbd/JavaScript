72cdf3927ef47730e940e0554f3c87e9015e2591
Irfanbd <gourob83@gmail.com>
2022-02-26 02:52:39 +0600

    Update tutorial_19.js

--- tutorial_19.js
+++ tutorial_19.js
@@ -1,5 +1,7 @@
+var m =parseInt( prompt("Enter Starting Point"));
+var n =parseInt( prompt("Enter Ending Point"));
 var sum = 0;
-for (var i = 1; i <= 10; i = i + 1){
+for (var i = m; i <= n; i = i + 1){
     sum = sum + i;
     // document.write(" Total : " + sum+"</br>");
    
